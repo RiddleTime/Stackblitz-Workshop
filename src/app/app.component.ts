a6947320d8c632cfbc8e8a2ece42c2aca8645a16
RiddleTime <reinierklarenberg@gmail.com>
2021-10-20 01:47:25 +0200

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,23 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
+
+    this.setCanvasSize();
+    this.drawGraphics();
+  }
+
+  private drawGraphics() {
+    this.ctx.fillStyle = 'FF0000';
+    this.ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+    this.ctx.fillStyle = 'FFFFFF';
     this.ctx.fillText('hello', 20, 20);
+    this.ctx.fillStyle = '00FF00';
+    this.ctx.arc(50, 50, 90, 0, 0);
+  }
+
+  private setCanvasSize() {
+    this.canvas.nativeElement.width = window.outerWidth;
+    this.canvas.nativeElement.height = window.outerHeight;
   }
 }
