6862a6c6b118c953c268e3f9382d7afc390472ec
RiddleTime <reinierklarenberg@gmail.com>
2021-10-20 21:28:59 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,73 +31,62 @@ export class AppComponent implements OnInit {
     }).appendTo(document.body);
   }
 
-  private tryTwo() {
-    var circle = this.two.makeCircle(-70, 0, 50);
-    var rect = this.two.makeRectangle(70, 0, 100, 100);
-    circle.fill = '#FF8000';
-    circle.stroke = 'orangered';
-    rect.fill = 'rgba(0, 200, 255, 0.75)';
-    rect.stroke = '#1C75BC';
-
-    // Groups can take an array of shapes and/or groups.
-    var group = this.two.makeGroup(circle, rect);
-
-    // And have translation, rotation, scale like all shapes.
-    group.translation.set(this.two.width / 2, this.two.height / 2);
-    group.rotation = Math.PI;
-    group.scale = 0.7;
-
-    // You can also set the same properties a shape have.
-    group.linewidth = 7;
-    this.two.add(group);
-
-    // Bind a function to scale and rotate the group
-    // to the animation loop.
-    this.two
-      .bind('update', function (frameCount: number) {
-        // This code is called everytime two.update() is called.
-        // Effectively 60 times per second.
-        if (group.scale > 0.9999) {
-          group.scale = group.rotation = 0;
-        }
-
-        var t = (1 - group.scale) * 0.125;
-        group.scale += t;
-        group.rotation += t * 4 * Math.PI;
-      })
-      .play(); // Finally, start the animation loop
-  }
-
   private addText() {
-    var styles = {
+    let styles = {
       family: 'proxima-nova, sans-serif',
       size: 50,
       leading: 50,
       weight: 900,
     };
-    var text = this.two.makeText(Math.round(Math.random() * 100), 0, 0, styles);
+    let text = new Two.Text(
+      Math.round(Math.random() * 100),
+      window.innerWidth / 2,
+      60,
+      styles
+    );
+    text.fill = '#000';
+    let topInfoGroup = this.two.makeGroup(text);
+    this.two.add(topInfoGroup);
 
-    var circle = this.two.makeCircle(window.innerWidth, window.innerHeight, 50);
+    let circleGroup = new Two.Group();
+    for (let i = 0; i < 5; i++) {
+      let circle = new Two.Circle(
+        window.innerWidth,
+        window.innerHeight,
+        (i + 1) * 80
+      );
+      circle.fill = '#00000077';
+      circle.stroke = 'red';
+
+      circleGroup.add(circle);
+    }
+    this.two.add(circleGroup);
+
+    let circleGroup2 = new Two.Group();
+    for (let i = 0; i < 5; i++) {
+      let circle = new Two.Circle(0, 0, (i + 1) * 80);
+      circle.fill = '#00000077';
+      circle.stroke = 'red';
+
+      circleGroup2.add(circle);
+    }
+    this.two.add(circleGroup2);
+
+    let circle = new Two.Circle(window.innerWidth, window.innerHeight, 50);
     circle.fill = '#00000077';
     circle.stroke = 'red';
 
-    var group = this.two.makeGroup(circle, text);
-    group.center();
-
-    this.two.add(group);
-
-    console.log(group.children);
-
     this.two
       .bind('update', (frameCount: number) => {
-        if (frameCount % 10 == 0)
-          if (group.scale < 2)
+        if (frameCount % 2 == 0)
+          if (circleGroup2.scale < 1)
             // text.rotation -= 0.01
-            group.scale += (1 - group.scale) * 0.125;
+            circleGroup2.scale += 0.01;
           else {
-            group.scale = 0;
-            group.value = Math.round(Math.random() * 100);
+            circleGroup2.scale = 0;
+            circleGroup2.value = Math.round(Math.random() * 100);
           }
+        this.two.un;
       })
       .play(); // Finally, start the animation loop
   }
