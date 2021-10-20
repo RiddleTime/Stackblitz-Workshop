d3447cbb8d7d7cf94a65ec620c876a5bae721105
RiddleTime <reinierklarenberg@gmail.com>
2021-10-21 00:00:27 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,21 +49,32 @@ export class AppComponent implements OnInit {
     this.two.add(topInfoGroup);
 
     let circles = new Two.Group();
+    let circleRadius = 70;
+
+    let circleTexts: any[] = [];
+
     for (let i = 0; i < 5; i++) {
       let group = new Two.Group();
 
-      let circle = new Two.Circle(0, 0, (i + 1) * 40);
+      let circle = new Two.Circle(0, 0, (i + 1) * circleRadius);
       circle.fill = '#00000077';
       circle.stroke = 'black';
+
+      let text = new Two.Text(
+        '?',
+        -1 * (i + 1) * circleRadius + circleRadius / 2,
+        -25,
+        {
+          family: 'proxima-nova, sans-serif',
+          size: 50,
+          leading: 0,
+          weight: 900,
+        }
+      );
+      text.fill = '#FFF';
+      circleTexts.push(text);
+
       group.add(circle);
-
-      let text = new Two.Text('+', (i + 1) * 40 - 20, 0, {
-        family: 'proxima-nova, sans-serif',
-        size: 50,
-        leading: 0,
-        weight: 900,
-      });
-
       group.add(text);
 
       circles.add(group);
@@ -78,14 +89,13 @@ export class AppComponent implements OnInit {
       .bind('update', (frameCount: number) => {
         if (frameCount % 2 == 0) {
           if (circles.scale < 1) {
-            circles.scale *= 1.02;
+            circles.scale *= 1.035;
           } else {
             circles.scale = 1;
-            circles.rotation -= 0.1;
+            circles.rotation += 0.01;
 
-            for (let i = 0; i < circles.children.count; i++) {
-              let textChild = circles.children[i].children[1];
-              textChild.rotation.set(textChild.rotation + 0.1);
+            for (let i = 0; i < circleTexts.length; i++) {
+              circleTexts[i].rotation -= 0.01;
             }
 
             text.value = Math.round(Math.random() * 100 + 1);
