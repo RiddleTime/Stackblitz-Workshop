f727ec07d16378b7fb59334bd9d7bd5cb7fa036d
RiddleTime <reinierklarenberg@gmail.com>
2021-10-20 16:32:35 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {
   ViewChild,
   OnInit,
 } from '@angular/core';
+import { AnalysisFailure } from '@angular/core/schematics/migrations/missing-injectable/transform';
 import Two from 'two.js';
 
 @Component({
@@ -13,10 +14,7 @@ import Two from 'two.js';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  @ViewChild('canvas', { static: true })
-  canvas: ElementRef<HTMLCanvasElement>;
-
-  private ctx: CanvasRenderingContext2D;
+  two;
 
   androidVersion = 'Angular ' + VERSION.major;
 
@@ -30,48 +28,75 @@ export class AppComponent implements OnInit {
   }
 
   private tryTwo() {
-    var two = new Two({
+    this.two = new Two({
       fullscreen: true,
       autostart: true,
     }).appendTo(document.body);
 
-    var circle = two.makeCircle(-70, 0, 50);
-    var rect = two.makeRectangle(70, 0, 100, 100);
+    var circle = this.two.makeCircle(-70, 0, 50);
+    var rect = this.two.makeRectangle(70, 0, 100, 100);
     circle.fill = '#FF8000';
     circle.stroke = 'orangered';
     rect.fill = 'rgba(0, 200, 255, 0.75)';
     rect.stroke = '#1C75BC';
 
     // Groups can take an array of shapes and/or groups.
-    var group = two.makeGroup(circle, rect);
+    var group = this.two.makeGroup(circle, rect);
 
     // And have translation, rotation, scale like all shapes.
-    group.translation.set(two.width / 2, two.height / 2);
+    group.translation.set(this.two.width / 2, this.two.height / 2);
     group.rotation = Math.PI;
     group.scale = 0.75;
 
     // You can also set the same properties a shape have.
     group.linewidth = 7;
-    two.add(group);
+    this.two.add(group);
 
     // Bind a function to scale and rotate the group
     // to the animation loop.
-    two
+    this.two
       .bind('update', function (frameCount) {
         // This code is called everytime two.update() is called.
         // Effectively 60 times per second.
         if (group.scale > 0.9999) {
           group.scale = group.rotation = 0;
         }
+
         var t = (1 - group.scale) * 0.125;
         group.scale += t;
         group.rotation += t * 4 * Math.PI;
       })
       .play(); // Finally, start the animation loop
+
+    this.addText();
   }
 
-  private setCanvasSize() {
-    this.canvas.nativeElement.width = window.outerWidth;
-    this.canvas.nativeElement.height = window.outerHeight;
+  private addText() {
+    var styles = {
+      family: 'proxima-nova, sans-serif',
+      size: 50,
+      leading: 50,
+      weight: 900,
+    };
+    var text = this.two.makeText(
+      'Text test yes',
+      window.innerWidth / 2,
+      window.innerHeight / 2,
+      styles
+    );
+
+    this.two
+      .bind('update', function (frameCount) {
+        // This code is called everytime two.update() is called.
+        // Effectively 60 times per second.
+        if (text.scale > 0.9999) {
+          text.scale = text.rotation = 0;
+        }
+
+        var t = (1 - text.scale) * 0.125;
+        text.scale += t;
+        text.rotation += t * -4 * Math.PI;
+      })
+      .play(); // Finally, start the animation loop
   }
 }
