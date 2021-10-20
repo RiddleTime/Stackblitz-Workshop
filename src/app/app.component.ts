e9809793964845ee6f3405ee39835e4ef424f55a
RiddleTime <reinierklarenberg@gmail.com>
2021-10-20 02:14:55 +0200

    5 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {
   ViewChild,
   OnInit,
 } from '@angular/core';
+import Two from 'two.js';
 
 @Component({
   selector: 'my-app',
@@ -17,23 +18,56 @@ export class AppComponent implements OnInit {
 
   private ctx: CanvasRenderingContext2D;
 
-  name = 'Angular ' + VERSION.major;
+  androidVersion = 'Angular ' + VERSION.major;
 
   ngOnInit(): void {
-    this.ctx = this.canvas.nativeElement.getContext('2d');
+    // this.ctx = this.canvas.nativeElement.getContext('2d');
 
-    this.setCanvasSize();
-    this.drawGraphics();
+    // this.setCanvasSize();
+    // this.drawGraphics();
+
+    this.tryTwo();
   }
 
-  private drawGraphics() {
-    this.ctx.fillStyle = 'FF0000';
-    this.ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+  private tryTwo() {
+    var two = new Two({
+      fullscreen: true,
+      autostart: true,
+    }).appendTo(document.body);
 
-    this.ctx.fillStyle = 'FFFFFF';
-    this.ctx.fillText('hello', 20, 20);
-    this.ctx.fillStyle = '00FF00';
-    this.ctx.arc(50, 50, 90, 0, 0);
+    var circle = two.makeCircle(-70, 0, 50);
+    var rect = two.makeRectangle(70, 0, 100, 100);
+    circle.fill = '#FF8000';
+    circle.stroke = 'orangered';
+    rect.fill = 'rgba(0, 200, 255, 0.75)';
+    rect.stroke = '#1C75BC';
+
+    // Groups can take an array of shapes and/or groups.
+    var group = two.makeGroup(circle, rect);
+
+    // And have translation, rotation, scale like all shapes.
+    group.translation.set(two.width / 2, two.height / 2);
+    group.rotation = Math.PI;
+    group.scale = 0.75;
+
+    // You can also set the same properties a shape have.
+    group.linewidth = 7;
+    two.add(group);
+
+    // Bind a function to scale and rotate the group
+    // to the animation loop.
+    two
+      .bind('update', function (frameCount) {
+        // This code is called everytime two.update() is called.
+        // Effectively 60 times per second.
+        if (group.scale > 0.9999) {
+          group.scale = group.rotation = 0;
+        }
+        var t = (1 - group.scale) * 0.125;
+        group.scale += t;
+        group.rotation += t * 4 * Math.PI;
+      })
+      .play(); // Finally, start the animation loop
   }
 
   private setCanvasSize() {
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h1>This is an example</h1>
+              <p> using {{androidVersion}}!</p>
+              <p>using <a href="https://two.js.org/">Two.js</a> </p>`,
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent {
+  @Input() androidVersion: string;
 }
