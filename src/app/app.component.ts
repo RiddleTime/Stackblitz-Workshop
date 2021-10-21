267a637a5b60425b92961e7fa6d80a65f4feda9c
RiddleTime <reinierklarenberg@gmail.com>
2021-10-21 02:32:17 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,12 +38,7 @@ export class AppComponent implements OnInit {
       leading: 50,
       weight: 900,
     };
-    let text = new Two.Text(
-      Math.round(Math.random() * 100 + 1),
-      window.innerWidth / 2,
-      60,
-      styles
-    );
+    let text = new Two.Text(0, window.innerWidth / 2, 60, styles);
     text.fill = '#000';
     let topInfoGroup = this.two.makeGroup(text);
     this.two.add(topInfoGroup);
@@ -100,11 +95,37 @@ export class AppComponent implements OnInit {
 
       interactable.on('move', (event) => {
         console.log(event);
-        console.log(event.velocity);
-        circleGroups.children[i - 1].rotation +=
-          event.movementY < 0 ? 0.15 : -0.15;
-        circleTexts[i - 1].rotation -= event.movementY < 0 ? 0.15 : -0.15;
+        console.log(event.speed);
+
         //  event.velocity / 100;
+      });
+
+      interactable.draggable({
+        // make the element fire drag events
+        origin: 'self', // (0, 0) will be the element's top-left
+        inertia: true, // start inertial movement if thrown
+        modifiers: [
+          interact.modifiers.restrict({
+            restriction: 'self', // keep the drag coords within the element
+          }),
+        ],
+        // Step 3
+        listeners: {
+          move(event) {
+            console.log(event);
+            let movement = -event.velocity.y / 40000 + event.velocityX / 40000;
+            console.log(movement);
+
+            circleGroups.children[i - 1].rotation += movement;
+            circleTexts[i - 1].rotation -= movement;
+            // call this listener on every dragmove
+            // const sliderWidth = interact.getElementRect(event.target).width;
+            // const value = event.pageX / sliderWidth;
+
+            // event.target.style.paddingLeft = value * 100 + '%';
+            // event.target.setAttribute('data-value', value.toFixed(2));
+          },
+        },
       });
     }
 
@@ -112,17 +133,19 @@ export class AppComponent implements OnInit {
       .bind('update', (frameCount: number) => {
         if (frameCount % 2 == 0) {
           for (let i = 0; i < circleGroups.children.length; i++) {
-            if (circleGroups.children[i].scale < 1) {
+            if (circleGroups.children[i].scale < 2) {
               circleGroups.children[i].scale *= 1.035;
             } else {
-              circleGroups.children[i].scale = 1;
+              circleGroups.children[i].scale = 2;
               // circleGroups.children[i].rotation += 0.01 * (i % 2 ? 1 : -1);
 
               // circleTexts[i].rotation -= 0.01 * (i % 2 ? 1 : -1);
             }
           }
+        }
 
-          text.value = Math.round(Math.random() * 100 + 1);
+        if (frameCount % 60 == 0) {
+          text.value++;
         }
       })
       .play(); // Finally, start the animation loop
